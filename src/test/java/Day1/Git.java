237f61f1566d22f43b661338d4d4c297a689d8d3
Ingenieur2000 <nurullahinmaili@gmail.com>
2022-03-20 20:27:11 +0100

    Kaya file Uploaded

--- src/test/java/Day1/Git.java
+++ src/test/java/Day1/Git.java
@@ -3,6 +3,7 @@ package Day1;
 public class Git {
     public static void main(String[] args) {
         System.out.println("git");
+        System.out.println("2. satır");
     }
 
 }
---
+++ src/test/java/Day1/Kaya.java
@@ -0,0 +1,7 @@
+package Day1;
+
+public class Kaya {
+
+    // bu file kaya bransını olustur
+
+}
